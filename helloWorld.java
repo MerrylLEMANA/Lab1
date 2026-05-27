fba0abdd4b2498091248a0d459501302cc204c7d
MerrylLEMANA <lilwizlemana@gmail.com>
2026-05-27 12:57:42 -0400

    Change message on conflict-demo

--- helloWorld.java
+++ helloWorld.java
@@ -1,5 +1,5 @@
 public class helloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello from conflict demo!");
+        System.out.println("Hello from nouveau conflict demo!");
     }
 }
